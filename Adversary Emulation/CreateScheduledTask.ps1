07854107ed6c886bf81b990bd3e78104e2d67206
ZeroDayZealot <95112988+LeMagicKonch@users.noreply.github.com>
2025-08-27 08:06:56 -0700

    Update CreateScheduledTask.ps1

--- Adversary Emulation/CreateScheduledTask.ps1
+++ Adversary Emulation/CreateScheduledTask.ps1
@@ -1,12 +1,10 @@
 $taskName = "Print Spooler"
 
-$taskPath = "PrintTask"
-
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -w hidden -c start-process calc.exe"
+$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -w hidden -c start-process C:\Windows\System32\calc.exe"
 
 $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date)
 
-Register-ScheduledTask -TaskName $taskName -TaskPath $taskPath -Action $action -Trigger $trigger -Force
+Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Force
 
 Start-Sleep -Seconds 5
 
