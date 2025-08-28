f755083e31c8b803c331f94d906535a8593ba3e4
ZeroDayZealot <95112988+LeMagicKonch@users.noreply.github.com>
2025-08-27 20:39:49 -0400

    Update taskSchedular.bat

--- Adversary Emulation/taskSchedular.bat
+++ Adversary Emulation/taskSchedular.bat
@@ -1,7 +1,8 @@
-schtasks /CREATE /SC MINUTE /TN updater /TR "powershell.exe -c calc.exe"
+schtasks /CREATE /SC MINUTE /TN updater /TR "powershell.exe -w hidden -c calc.exe; schtasks /DELETE /TN updater /F"
 
 timeout /t 240
 
 schtasks /DELETE /TN updater /F
+
 
 
